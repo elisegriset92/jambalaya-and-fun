1ef1a1bd1988a339f60ee0797446cefefcdc25c5
Elise Griset <elise.griset@gmail.com>
2018-04-12 19:48:42 +0200

    done with pokemon

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-const express = require("express")
-const app = express()
-const port = 3000
-const poke = require("./pokeService")
+const express = require('express');
+const app = express();
+const port = 3000;
+const poke = require('./pokeService');
+const hbs = require('hbs');
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+app.set('layout', __dirname + '/views/layout.hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.get('/', (req, res, next) => {
+  res.render('home');
+});
+
+app.get('/random', (req, res, next) => {
+  const promise = poke.random();
+
+  promise.then(pokemonObject => {
+    res.render('random', {name: pokemonObject.name, imageUrl: pokemonObject.sprites.back_default});
+  });
+});
 /*
 * Let's write our route handlers here!
-* 
 */
 
-
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
-    poke.random().then((res) => { console.log(res.name) })
-})
+  console.log(`Server listening on port ${port}`);
+  poke.random().then(res => {
+    console.log(res.name);
+  });
+});
