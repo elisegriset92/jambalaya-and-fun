f263810f24abf0af9952873742d86e2b320286ac
Michael Lande Blau <miclande@gmail.com>
2018-04-12 17:04:04 +0200

    pushing solution

--- app.js
+++ app.js
@@ -3,14 +3,33 @@ const app = express()
 const port = 3000
 const poke = require("./pokeService")
 
-/*
-* Let's write our route handlers here!
-* 
-*/
+const hbs = require('hbs');
 
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.set('view options', { layout: 'main-layout' });
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.get("/random", (req, res) => {
+    poke.random().then(pokemonObject => {
+        data = {
+            pokemon:
+                {
+                    name: pokemonObject.name,
+                    imageUrl: pokemonObject.sprites.back_default,
+                    types: pokemonObject.types.map(t => t.type)
+                }
+        }
+        res.render("random", data)
+    })
+})
 // SOLUTION
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
-    poke.random().then((res) => { console.log(res.name) })
 })
